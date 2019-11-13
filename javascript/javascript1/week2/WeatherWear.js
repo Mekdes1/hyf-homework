a7464d86b2a0f52e07f5847e1628001b2ac3f296
Mekdes <yesmek@yahoo.com>
2019-11-13 17:54:42 +0100

    commiting WeatherWear.js

---
+++ javascript/javascript1/week2/WeatherWear.js
@@ -0,0 +1,23 @@
+/* Weather wear */
+
+let recommandation;
+function wetherWere(temprature){
+     if(temprature <= 6) {
+        recommandation = "its very cold. go for winter cloth";
+        return recommandation
+    } else if 
+        (temprature >6 && temprature < 18) { 
+        recommandation ="put on some light jacket";
+        return recommandation
+} else if 
+ (temprature >18 && temprature <27 ) {
+     recommandation = "it is getting warm put some light cloth on";
+
+} else if 
+(temprature > 27) {
+    recommandation = "it is very warm out there";
+
+}
+    }
+wetherWere (25);
+console .log(`${recommandation}`);
