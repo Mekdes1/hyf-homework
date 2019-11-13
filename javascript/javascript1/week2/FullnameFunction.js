d891c8809b634947d1f8a7483b51692915ff0fed
Mekdes <yesmek@yahoo.com>
2019-11-13 17:49:03 +0100

    commited FullnameFunction.js

---
+++ javascript/javascript1/week2/FullnameFunction.js
@@ -0,0 +1,20 @@
+/* Flight booking fullname function */                        
+
+ function getFullname(firstname,surename,useFormalName) {
+ if (useFormalName == true) {
+
+    let displayFormalName = "Lord "+ " " + firstname + " " + surename 
+    return displayFormalName;
+} else {
+    
+    let displayFullName = firstname + " " + surename
+    return displayFullName;
+}
+
+}
+const fullname1 = "Mekdes";
+const fullname2 = "Habtamu";
+useFormalName = true;
+
+const fullname = getFullname(fullname1,fullname2,true);
+console .log(`${fullname}`); 
