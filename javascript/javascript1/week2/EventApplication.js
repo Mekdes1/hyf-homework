64884a88553cd1fb1d0705834a0ecdcc746c1215
Mekdes <yesmek@yahoo.com>
2019-11-13 17:53:52 +0100

    commited EventApplication.js

---
+++ javascript/javascript1/week2/EventApplication.js
@@ -0,0 +1,19 @@
+/* Event application */ 
+
+const TheDayToday = new Date();
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let weekDate = TheDayToday.getDay();
+let nextEventweekDay;
+
+function getEventWeekDay (eventDay) {
+
+    nextEventweekDay = daysOfTheWeek [(eventDay + weekDate) % 7 ];
+    return nextEventweekDay;
+}
+
+getEventWeekDay (2)
+console .log (`the event will be on:  ${nextEventweekDay}`);
+
+
+
+
