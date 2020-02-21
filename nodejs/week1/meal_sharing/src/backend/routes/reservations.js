9b753dd50a6510e0a636e05e8b4699203cc81fcf
Mekdes <yesmek@yahoo.com>
2020-02-21 15:34:38 +0100

    committed changes

--- nodejs/week1/meal_sharing/src/backend/routes/reservations.js
+++ nodejs/week1/meal_sharing/src/backend/routes/reservations.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const router = express();
-
-
-
-
 const reservations = require("../data/reservations.json");
 
-router.get("/reservations",(request,responsse) => {
-    responsse.json(reservations);
-})
+router.get("/reservations", (request, responsse) => {
+  responsse.json(reservations);
+});
 
-
-  module.exports = router;
+module.exports = router;
